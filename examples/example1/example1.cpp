7f414ac74b93bae1c6894982a2e8abee6a702b12
leaf-woods <lin.ye.020603@gmail.com>
2025-08-16 10:43:34 -0600

    Change function name getContents(), getColorValueTreeContents() to writeContents(), writeColorValueTreeContents(). Change function setMinMax() to private to

--- examples/example1/example1.cpp
+++ examples/example1/example1.cpp
@@ -63,7 +63,6 @@ int main(int argc, char* argv[])
     desc.setDescData(input);
     desc.printMap();
     desc.printColorValueTree();
-    desc.setMinMax();
     desc.printMinMax();
     cout << endl;
     //*/
