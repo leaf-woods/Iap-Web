b4c05e40b6be039e43c3ee865a416941b602e924
leaf-woods <lin.ye.020603@gmail.com>
2025-08-18 14:48:56 -0600

    Issue 8

---
+++ examples/example4/calcBgrHsv.h
@@ -0,0 +1,18 @@
+#ifndef CALCBGRHSV_H
+#define CALCBGRHSV_H
+
+#include <opencv2/core.hpp>
+
+class calcBgrHsv{
+
+    public:
+      calcBgrHsv();
+      ~calcBgrHsv();
+
+      double diffBGR(uchar b1, uchar g1, uchar r1, uchar b2, uchar g2, uchar r2);
+      double diffHSV(int h1, int s1, int v1, int h2, int s2, int v2);
+      void toHsvCV(int* hsv, double b, double g, double r);
+      
+}; 
+#endif
+
