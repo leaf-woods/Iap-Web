44bae7304b22ca3b22b19ce959ddd4642b5b3595
leaf-woods <lin.ye.020603@gmail.com>
2025-08-18 19:48:53 -0600

    Improved efficency: calcBgrHsv.cpp::toHsvCV()

--- examples/example4/calcBgrHsv.h
+++ examples/example4/calcBgrHsv.h
@@ -11,7 +11,7 @@ class calcBgrHsv{
 
       double diffBGR(uchar b1, uchar g1, uchar r1, uchar b2, uchar g2, uchar r2);
       double diffHSV(int h1, int s1, int v1, int h2, int s2, int v2);
-      void toHsvCV(int* hsv, double b, double g, double r);
+      void toHsvCV(int* hsv, uchar b, uchar g, uchar r);
       
 }; 
 #endif
