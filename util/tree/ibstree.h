1ab1bf8e9a1534e5a20046b71b87394bc8b8119e
leaf-woods <lin.ye.020603@gmail.com>
2025-08-22 15:10:50 -0600

    Prod tree package

---
+++ util/tree/ibstree.h
@@ -0,0 +1,53 @@
+#ifndef IBSTREE_H
+#define IBSTREE_H
+
+#include "ITreeNode.h"
+#include "iap_print.h"
+
+class ibstree {
+    public:
+      virtual void deleteTree()=0; 
+      virtual int getMaxValue()=0;
+      virtual int size()=0;
+      virtual void printTree()=0;
+};
+
+// Every child class type of abstree has its own root.
+class abstree : public ibstree {
+    protected:
+      const static int DEBUG = 1;
+      const std::string DEBUG_H;
+
+      int count;
+
+      iap_print* printer;
+
+    public:
+      abstree() : DEBUG_H("Debug - ") {}
+      virtual ~abstree(){};
+
+      int size() { return count;}
+      void setPrint(iap_print* p) { printer = p;}
+
+    protected:
+      template<typename N>
+      int getMaxValue(N* n) {
+          if (n == nullptr) {
+            return 0;
+          } 
+          if (n->right == nullptr) {
+            return n->data;
+          }
+          int count = 0;
+          int value = n->data;
+          // Find right most leaf on the tree
+          while (n->right != nullptr) {
+            value = n->right->data; 
+            n = n->right;
+            count++;
+          }
+          return value;
+      }
+};
+
+#endif
